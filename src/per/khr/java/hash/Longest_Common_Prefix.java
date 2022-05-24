9fb4db31416bc1f6e103f21f08d231968a714009
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-05-24 23:03:21 +0900

    commit

--- src/per/khr/java/hash/Longest_Common_Prefix.java
+++ src/per/khr/java/hash/Longest_Common_Prefix.java
@@ -2,6 +2,18 @@ package per.khr.java.hash;
 
 public class Longest_Common_Prefix {
     public String longestCommonPrefix(String[] strs) {
+        String result = "";
+
+        int count = strs[0].length();
+
+        for (int i = 0; i < count; ++i) {
+
+            Character diff;
+            for (int j = 0; j < strs.length; ++j) {
+                if(diff == null)
+            }
+        }
+
         return null;
     }
 }
