04a5b40a068f29f1e6d0d8f6c46cbee252c8258d
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-05-24 20:56:01 +0900

    commit

--- src/per/khr/main/hash/Two_Sum.java
+++ src/per/khr/main/hash/Two_Sum.java
@@ -1,7 +1,11 @@
 package per.khr.main.hash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Two_Sum {
     public static int[] main(String[] args) {
+        // 내 답안 169ms
         int[] nums = {2, 7, 11, 15};
         int target = 9;
 
@@ -17,5 +21,18 @@ public class Two_Sum {
                 if (result[0] + result[1] == target) return new int[]{idx - 1, idx2};
             }
         }
+
+//        빠른 답안(HashMap 사용..)
+//        int[] result = new int[2];
+//        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if (map.containsKey(target - nums[i])) {
+//                result[1] = i;
+//                result[0] = map.get(target - nums[i]);
+//                return result;
+//            }
+//            map.put(nums[i], i);
+//        }
+//        return result;
     }
 }
