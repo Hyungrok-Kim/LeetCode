30d0accaffe0121c4eeed94ccf08c72f74a2881f
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-05-25 21:40:28 +0900

    commit

--- src/per/khr/java/hash/Longest_Common_Prefix.java
+++ src/per/khr/java/hash/Longest_Common_Prefix.java
@@ -2,6 +2,7 @@ package per.khr.java.hash;
 
 public class Longest_Common_Prefix {
     public String longestCommonPrefix(String[] strs) {
+        // 내 답안
         String result = "";
 
         Character before;
@@ -20,5 +21,29 @@ public class Longest_Common_Prefix {
 
             result += strs[0].charAt(i);
         }
+
+        // 멋진 답안
+//        if (strs == null || strs.length == 0) {
+//            return "";
+//        }
+        // 1) string builder
+//        StringBuilder result = new StringBuilder();
+//
+//        // Sort array
+//        Arrays.sort(strs);
+//
+//        // split first and last to char
+//        char[] first = strs[0].toCharArray();
+//        char[] last = strs[strs.length - 1 ].toCharArray();
+//
+//        // loop through
+//        for (int i = 0; i < first.length && i < last.length; i++) {
+//            if (first[i] == last[i]) {
+//                result.append(first[i]);
+//            }
+//            else
+//                return result.toString();
+//        }
+//        return result.toString();
     }
 }
