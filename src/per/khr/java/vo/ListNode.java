f62fc74129ebb44fa223cd64b3d616057dc15487
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-05-26 23:11:23 +0900

    commit

--- src/per/khr/java/vo/ListNode.java
+++ src/per/khr/java/vo/ListNode.java
@@ -1,8 +1,8 @@
 package per.khr.java.vo;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     ListNode() {
     }
--- src/test/Merge_Two_Sorted_ListsTest.java
+++ src/test/Merge_Two_Sorted_ListsTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
-import per.khr.java.Else.Merge_Two_Sorted_Lists;
+import per.khr.java.Queue.Merge_Two_Sorted_Lists;
 import per.khr.java.vo.ListNode;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Merge_Two_Sorted_ListsTest {
 
@@ -9,8 +11,13 @@ public class Merge_Two_Sorted_ListsTest {
         Merge_Two_Sorted_Lists mtsl = new Merge_Two_Sorted_Lists();
 
         ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-        ListNode list2 = new ListNode(1, new ListNode(2, new ListNode(3)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode result = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))));
 
-        mtsl.mergeTwoLists(list1, list2);
+        ListNode list3 = new ListNode(5);
+        ListNode list4 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode result2 = new ListNode(1, new ListNode(2, new ListNode(4, new ListNode(5))));
+
+        assertThat(mtsl.mergeTwoLists(list3, list4)).isEqualTo(result2);
     }
 }
