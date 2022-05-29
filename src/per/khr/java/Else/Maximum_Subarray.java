b4bd6521649a03619d2aab2ad06e6d9cbc77813a
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-05-29 23:06:45 +0900

    commit

--- src/per/khr/java/Else/Maximum_Subarray.java
+++ src/per/khr/java/Else/Maximum_Subarray.java
@@ -1,7 +1,16 @@
 package per.khr.java.Else;
 
 public class Maximum_Subarray {
-    public int maxSubArray(int[] nums) {
 
+    public int maxSubArray(int[] nums) {
+        boolean[] visited  = new boolean[nums.length];
+
+        for (int i = 0; i < nums.length; ++i) {
+            visited[i] = true;
+            // 재귀
+
+        }
+
+        return 0;
     }
 }
