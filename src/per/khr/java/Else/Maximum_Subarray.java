36560473e52c210aff0c18ddb71c0a5f4491a34a
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-05-30 22:55:54 +0900

    Commit

--- src/per/khr/java/Else/Maximum_Subarray.java
+++ src/per/khr/java/Else/Maximum_Subarray.java
@@ -1,17 +1,21 @@
 package per.khr.java.Else;
 
+// O(n) 풀이
+// 카데인 알고리즘
+// 카데인 알고리즘이란
+// - 이전요소의 부분합을 알면 현재요소의 최대값을 알 수 있는 알고리즘.
 public class Maximum_Subarray {
     public int maxSubArray(int[] nums) {
-        int globalMax = Integer.MIN_VALUE;
-        int currentMax = 0;
+        int bestSum = Integer.MIN_VALUE;
+        int curSum = 0;
 
         for (int i = 0; i < nums.length; ++i) {
-            currentMax = Math.max(nums[i], currentMax + nums[i]);
-            globalMax = Math.max(globalMax, currentMax);
+            curSum = Math.max(nums[i], curSum + nums[i]);
+            bestSum = Math.max(bestSum, curSum);
         }
         // currentMax 현재 idx의 숫자와 지금까지 더해왔던 것 중 가장 큰 값을 비교
         // globalMax는 "지금까지 더해왔던 것 중 가장 큰 값" 과 "지금까지 더해왔던 것 중 가장 큰 값에 현재 idx를 더한 값을 비교"
 
-        return globalMax;
+        return bestSum;
     }
 }
