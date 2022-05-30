4632921128d4c83ba420ba48a4a66b9231305979
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-05-30 22:34:51 +0900

    commit

--- src/per/khr/java/Else/Maximum_Subarray.java
+++ src/per/khr/java/Else/Maximum_Subarray.java
@@ -2,28 +2,15 @@ package per.khr.java.Else;
 
 import java.util.TreeSet;
 
-// 아무래도 Dynimic programming 으로 바꿔야할듯...
 public class Maximum_Subarray {
-    private TreeSet<Integer> ts = new TreeSet<>();
-
-    public void recursive(int startIdx, int[] nums, int plus) {
-        if(startIdx + plus >= nums.length) return ;
-
-        int startFixed = startIdx;
-        int sum = 0;
-        for(int i = startIdx; i <= startIdx + plus; ++i) {
-            sum += nums[i];
-        }
-
-        ts.add(sum);
-        recursive(startFixed, nums, plus + 1);
-    }
-
     public int maxSubArray(int[] nums) {
         int plus = 0;
         for (int i = 0; i < nums.length; ++i) {
-            recursive(i, nums, plus);
+            recursive(i, nums, 0);
         }
+
+        int globalMax = Integer.MIN_VALUE;
+        System.out.println(globalMax);
 
         return ts.last();
     }
