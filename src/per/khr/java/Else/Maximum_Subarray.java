ca12825b86fbeed707ccd576418c386e01a39957
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-05-30 22:52:54 +0900

    commit

--- src/per/khr/java/Else/Maximum_Subarray.java
+++ src/per/khr/java/Else/Maximum_Subarray.java
@@ -1,17 +1,17 @@
 package per.khr.java.Else;
 
-import java.util.TreeSet;
-
 public class Maximum_Subarray {
     public int maxSubArray(int[] nums) {
-        int plus = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            recursive(i, nums, 0);
-        }
-
         int globalMax = Integer.MIN_VALUE;
-        System.out.println(globalMax);
+        int currentMax = 0;
 
-        return ts.last();
+        for (int i = 0; i < nums.length; ++i) {
+            currentMax = Math.max(nums[i], currentMax + nums[i]);
+            globalMax = Math.max(globalMax, currentMax);
+        }
+        // currentMax 현재 idx의 숫자와 지금까지 더해왔던 것 중 가장 큰 값을 비교
+        // globalMax는 "지금까지 더해왔던 것 중 가장 큰 값" 과 "지금까지 더해왔던 것 중 가장 큰 값에 현재 idx를 더한 값을 비교"
+
+        return globalMax;
     }
 }
