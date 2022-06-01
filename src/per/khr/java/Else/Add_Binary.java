a588485a4aac966fd8f460f235ddb64a2d6dd493
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-01 23:41:49 +0900

    Commit

--- src/per/khr/java/Else/Add_Binary.java
+++ src/per/khr/java/Else/Add_Binary.java
@@ -1,10 +1,23 @@
 package per.khr.java.Else;
 
 public class Add_Binary {
-    public String addBinary(String a, String b) {
-        Integer aToDecimal = Integer.parseInt(a, 2);
-        Integer bToDecimal = Integer.parseInt(b, 2);
+    //    public String addBinary(String a, String b) {
+//        long aToDecimal = Long.valueOf(a, 2);
+//        long bToDecimal = Long.valueOf(b, 2);
+//
+//        return Long.toBinaryString(aToDecimal + bToDecimal);
+//    }
 
-        return Integer.toBinaryString( aToDecimal + bToDecimal);
+    public String addBinary(String a, String b) {
+        int lenDiff = a.length() - b.length();
+
+        char[] aCArr = a.toCharArray();
+        char[] bCArr = b.toCharArray();
+
+        for (int i = aCArr.length - 1; i >= 0; --i) {
+            aCArr[i] += bCArr[i - lenDiff];
+        }
+
+        return null;
     }
 }
