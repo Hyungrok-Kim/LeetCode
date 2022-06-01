48dfbd07ba98174bdd6821a1f5c8bd90869f931f
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-01 13:23:39 +0900

    Commit

--- src/test/Plus_OneTest.java
+++ src/test/Plus_OneTest.java
@@ -12,5 +12,7 @@ public class Plus_OneTest {
         Plus_One po = new Plus_One();
 
         assertThat(po.plusOne(new int[] {1,2,3})).isEqualTo(new int[] {1,2,4});
+        assertThat(po.plusOne(new int[] {4,3,2,1})).isEqualTo(new int[] {4,3,2,2});
+        assertThat(po.plusOne(new int[] {9})).isEqualTo(new int[] {1,0});
     }
 }
