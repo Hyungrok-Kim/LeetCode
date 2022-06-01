9d6e81c9832878d4482b859d03d7bf3651f1f88b
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-01 14:52:41 +0900

    Commit

---
+++ src/test/RunningSumOf1DArrayTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import per.khr.java.DynamicProgramming.RunningSumOf1DArray;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RunningSumOf1DArrayTest {
+
+    @Test
+    public void runningSum() {
+        RunningSumOf1DArray rsoa = new RunningSumOf1DArray();
+
+        assertThat(rsoa.runningSum(new int[]{1, 2, 3, 4})).isEqualTo(new int[]{1, 3, 6, 10});
+        assertThat(rsoa.runningSum(new int[]{1, 1, 1, 1})).isEqualTo(new int[]{1, 2, 3, 4, 5});
+    }
+}
