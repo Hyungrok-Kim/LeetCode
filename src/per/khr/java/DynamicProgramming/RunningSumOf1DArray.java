fc6a9238695690d39af9007107847fe333df7d8d
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-01 15:06:11 +0900

    Commit

--- src/per/khr/java/DynamicProgramming/RunningSumOf1DArray.java
+++ src/per/khr/java/DynamicProgramming/RunningSumOf1DArray.java
@@ -2,9 +2,9 @@ package per.khr.java.DynamicProgramming;
 
 public class RunningSumOf1DArray {
 
-    public int getBeforeSum(int idx, int[] runningSum) {
-        if (runningSum[idx - 1] == 0 && idx - 1 != 0) {
-            runningSum[idx - 1] = getBeforeSum(idx - 1, runningSum);
+    public int getBeforeSum(int idx, int[] runningSum, boolean[] allotted) {
+        if (!allotted[idx - 1] && idx - 1 != 0) {
+            runningSum[idx - 1] = getBeforeSum(idx - 1, runningSum, allotted);
         }
 
         return runningSum[idx - 1];
@@ -12,10 +12,12 @@ public class RunningSumOf1DArray {
 
     public int[] runningSum(int[] nums) {
         int[] runningSum = new int[nums.length];
+        boolean[] allotted = new boolean[nums.length];
 
         runningSum[0] = nums[0];
         for (int i = 1; i < runningSum.length; ++i) {
-            runningSum[i] = getBeforeSum(i, runningSum) + nums[i];
+            runningSum[i] = getBeforeSum(i, runningSum, allotted) + nums[i];
+            allotted[i] = true;
         }
 
         return runningSum;
--- src/test/RunningSumOf1DArrayTest.java
+++ src/test/RunningSumOf1DArrayTest.java
@@ -10,6 +10,6 @@ public class RunningSumOf1DArrayTest {
         RunningSumOf1DArray rsoa = new RunningSumOf1DArray();
 
         assertThat(rsoa.runningSum(new int[]{1, 2, 3, 4})).isEqualTo(new int[]{1, 3, 6, 10});
-        assertThat(rsoa.runningSum(new int[]{1, 1, 1, 1})).isEqualTo(new int[]{1, 2, 3, 4, 5});
+        assertThat(rsoa.runningSum(new int[]{1, 1, 1, 1, 1})).isEqualTo(new int[]{1, 2, 3, 4, 5});
     }
 }
