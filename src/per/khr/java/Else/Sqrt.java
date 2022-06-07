b581a6b18e20809ad8124f307fdc66eed08a9efc
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-07 23:06:11 +0900

    commit

---
+++ src/per/khr/java/Else/Sqrt.java
@@ -0,0 +1,22 @@
+package per.khr.java.Else;
+
+public class Sqrt {
+    // method 사용 solution
+//    public int mySqrt(int x) {
+//        return (int)Math.sqrt(x);
+//    }
+
+    // method not use solution
+    public int mySqrt(int x) {
+        if (x == 1) return 1;
+
+        for (int i = 46340; i <= x / 2; ++i) {
+            int pre = i * i;
+            int post = (i + 1) * (i + 1);
+
+            if (x >= pre && (x < post || post < 0)) return i;
+        }
+
+        return 0;
+    }
+}
---
+++ src/test/SqrtTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import per.khr.java.Else.Sqrt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SqrtTest {
+
+    @Test
+    public void mySqrt() {
+        Sqrt s = new Sqrt();
+
+//        assertThat(s.mySqrt(4)).isEqualTo(2);
+//        assertThat(s.mySqrt(8)).isEqualTo(2);
+        assertThat(s.mySqrt(2147395600)).isEqualTo(46340);
+    }
+}
