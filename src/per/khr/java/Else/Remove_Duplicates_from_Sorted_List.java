0e6b8fd05f85e00b64f4c9e9a85e94a2311c0ce5
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-14 21:10:09 +0900

    Commit

--- src/per/khr/java/Else/Remove_Duplicates_from_Sorted_List.java
+++ src/per/khr/java/Else/Remove_Duplicates_from_Sorted_List.java
@@ -1,8 +1,20 @@
 package per.khr.java.Else;
+
 import per.khr.java.vo.ListNode;
 
 public class Remove_Duplicates_from_Sorted_List {
     public ListNode deleteDuplicates(ListNode head) {
+        ListNode target = head.next;
+        while (true) {
+            if (target == null) break;
+
+            if (head.val == target.val) {
+                head = target;
+                target = head.next;
+            } else {
+                target =
+            }
+        }
 
         return null;
     }
