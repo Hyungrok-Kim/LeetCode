23b482bedff0b6604864df181fe3c1f9c95ba660
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-14 21:50:05 +0900

    Commit

--- src/per/khr/java/Else/Remove_Duplicates_from_Sorted_List.java
+++ src/per/khr/java/Else/Remove_Duplicates_from_Sorted_List.java
@@ -2,20 +2,34 @@ package per.khr.java.Else;
 
 import per.khr.java.vo.ListNode;
 
+import java.util.Iterator;
+import java.util.TreeSet;
+
+// 미완성
 public class Remove_Duplicates_from_Sorted_List {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode target = head.next;
-        while (true) {
-            if (target == null) break;
+        TreeSet<Integer> ts = new TreeSet<>();
 
-            if (head.val == target.val) {
-                head = target;
-                target = head.next;
-            } else {
-                target =
-            }
+        while (head.next != null) {
+            ts.add(head.val);
+
+            head = head.next;
         }
 
+        Iterator i = ts.iterator();
+        ListNode resultNode = new ListNode();
+        while (i.hasNext()) {
+            int target = (int) i.next();
+            resultNode.val = target;
+
+            if (i.hasNext()) {
+                int nextTarget = (int) i.next();
+
+                if (target != nextTarget) {
+                    resultNode.next = new ListNode(nextTarget);
+                }
+            }
+        }
         return null;
     }
 }
