b9704688a1a6ee83498b1e7c884c5f1d55e0a338
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-24 21:15:51 +0900

    EAZY -> 104. Maximum Depth of Binary Tree

--- src/per/khr/java/Else/Maximum_Depth_of_Binary_Tree.java
+++ src/per/khr/java/Else/Maximum_Depth_of_Binary_Tree.java
@@ -1,4 +1,24 @@
 package per.khr.java.Else;
 
+import per.khr.java.vo.TreeNode;
+
+import java.util.TreeSet;
+
 public class Maximum_Depth_of_Binary_Tree {
+    TreeSet<Integer> ts = new TreeSet<>();
+
+    public void searchDepth(TreeNode target, int depth) {
+        if (target.left == null && target.right == null) ts.add(depth);
+        else {
+            if (target.left != null) searchDepth(target.left, depth + 1);
+            if (target.right != null) searchDepth(target.right, depth + 1);
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        searchDepth(root, 1);
+
+        return ts.pollLast();
+    }
 }
