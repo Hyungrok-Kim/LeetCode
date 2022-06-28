5b813123d8b1b516d485edce26cc3dfd84789d84
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-28 21:59:34 +0900

    Medium -> 33. Search in Rotated Sorted Array

--- src/per/khr/java/BinarySearch/Search_in_Rotated_Sorted_Array.java
+++ src/per/khr/java/BinarySearch/Search_in_Rotated_Sorted_Array.java
@@ -7,38 +7,57 @@ public class Search_in_Rotated_Sorted_Array {
 //        target이 0번 부터 ~ 0의 위치에 속하는 지
 //        target이 0의 위치부터 ~ 마지막 값에 속하는 지 알아낸 후에는
 //        평범한 이분 탐색으로 풀 수 있을 듯
+        if (nums.length == 0) return -1;
+        if (nums.length == 1 && nums[0] != target) return -1;
 
-//        가장 큰 수를 찾아야곘네
+        boolean rotated = false;
+
         int maxNum = Integer.MIN_VALUE;
         int maxNumIdx = 0;
 
         for (int i = 0; i < nums.length; ++i) {
-            if (maxNum < nums[i]) {
+//            if (maxNum < nums[i]) {
+//                maxNum = nums[i];
+//                maxNumIdx = i;
+//            }
+
+            if (maxNum > nums[i]) {
+                rotated = true;
+                break;
+            } else {
                 maxNum = nums[i];
                 maxNumIdx = i;
             }
         }
-
         int startIdx = 0, endIdx = nums.length - 1, zeroLocation = Integer.MAX_VALUE, result = 0;
+
+//        if (nums[0] == target) return 0;
+//        if (nums[maxNumIdx] == target) return maxNumIdx;
+
+//        if (nums[maxNumIdx + 1] == target) return maxNumIdx + 1;
+//        if (nums[nums.length - 1] == target) return nums.length - 1;
+
+        if (rotated) {
+            if (nums[0] <= target && nums[maxNumIdx] >= target) endIdx = maxNumIdx;
+            else if (nums[nums.length - 1] >= target) startIdx = maxNumIdx + 1;
+            else return -1;
+        }
 
         while (endIdx - startIdx != 0) {
             int targetIdx = (startIdx + endIdx) / 2;
 
-            if (nums[targetIdx] == 0) {
-                zeroLocation = targetIdx;
-                break;
+            if (nums[targetIdx] == target) {
+                return targetIdx;
             } else if (nums[targetIdx] > target) {
-                endIdx = target;
+                endIdx = targetIdx;
             } else if (nums[targetIdx] < target) {
-                startIdx = target;
+                startIdx = targetIdx;
             }
         }
 
-        if (nums[startIdx] == 0) zeroLocation = startIdx;
-        if (nums[endIdx] == 0) zeroLocation = endIdx;
+        if (nums[startIdx] == target) return startIdx;
+        if (nums[endIdx] == target) return endIdx;
 
-        if (zeroLocation > 5000) return -1;
-
-        return result;
+        return -1;
     }
 }
---
+++ src/test/Search_in_Rotated_Sorted_ArrayTest.java
@@ -0,0 +1,18 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+import per.khr.java.BinarySearch.Search_in_Rotated_Sorted_Array;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class Search_in_Rotated_Sorted_ArrayTest extends TestCase {
+
+    @Test
+    public void testSearch() {
+        Search_in_Rotated_Sorted_Array sirsa = new Search_in_Rotated_Sorted_Array();
+
+//        assertThat(sirsa.search(new int[] {1,3}, 0)).isEqualTo(-1);
+        assertThat(sirsa.search(new int[] {4,5,6,7,0,1,2}, 0)).isEqualTo(4);
+        assertThat(sirsa.search(new int[] {1}, 0)).isEqualTo(-1);
+        assertThat(sirsa.search(new int[] {4,5,6,7,0,1,2}, 3)).isEqualTo(-1);
+    }
+}
