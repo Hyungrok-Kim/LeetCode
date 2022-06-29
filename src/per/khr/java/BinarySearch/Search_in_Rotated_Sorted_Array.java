63f2f9f6283ecb61c9f6458a996b360abe75824b
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-29 23:41:21 +0900

    COMMIT

--- src/per/khr/java/BinarySearch/Search_in_Rotated_Sorted_Array.java
+++ src/per/khr/java/BinarySearch/Search_in_Rotated_Sorted_Array.java
@@ -1,12 +1,14 @@
 package per.khr.java.BinarySearch;
 
+/**
+ * 0을 먼저 찾아서
+ * nums의 0번 값과 마지막 값을 비교해서
+ * target이 0번부터 ~ 0의 위치에 속하는 지
+ * target이 0의 위치부터 ~ 마지막 값에 속하는 지 알아낸 후에는
+ * 평범한 이분 탐색으로 풀 수 있을 듯
+ */
 public class Search_in_Rotated_Sorted_Array {
     public int search(int[] nums, int target) {
-//        0을 먼저 찾아서
-//        nums의 0번 값과 마지막 값을 비교 해서
-//        target이 0번 부터 ~ 0의 위치에 속하는 지
-//        target이 0의 위치부터 ~ 마지막 값에 속하는 지 알아낸 후에는
-//        평범한 이분 탐색으로 풀 수 있을 듯
         if (nums.length == 0) return -1;
         if (nums.length == 1 && nums[0] != target) return -1;
 
