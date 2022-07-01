f1cd48a739b400f8e555ccff0a95a3e3ff89bc54
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-07-01 22:30:27 +0900

    Medium -> 81. Search in Rotated Sorted Array 2

--- src/per/khr/java/BinarySearch/Search_in_Rotated_Sorted_Array_2.java
+++ src/per/khr/java/BinarySearch/Search_in_Rotated_Sorted_Array_2.java
@@ -1,7 +1,5 @@
 package per.khr.java.BinarySearch;
 
-import java.util.Arrays;
-
 /**
  * 우선 0의 위치를 찾고 target이 첫번째 인덱스부터 0의 -1 위치까지에 속하는 지
  * 그 다음에 속하는 지를 알아야 한다.
@@ -10,8 +8,37 @@ import java.util.Arrays;
  * 하지만 찾는 숫자의 index를 요구한다면?
  */
 public class Search_in_Rotated_Sorted_Array_2 {
+    //    public boolean search(int[] nums, int target) {
+//        Arrays.sort(nums);
+//        int startIdx = 0, endIdx = nums.length - 1, targetIdx = 0;
+//
+//        while (startIdx <= endIdx) {
+//            targetIdx = startIdx + (endIdx - startIdx) / 2;
+//
+//            if (nums[targetIdx] == target) {
+//                return true;
+//            } else if (nums[targetIdx] > target) {
+//                endIdx = targetIdx - 1;
+//            } else if (nums[targetIdx] < target) {
+//                startIdx = targetIdx + 1;
+//            }
+//        }
+//
+//        return false;
+//    }
     public boolean search(int[] nums, int target) {
-        Arrays.sort(nums);
+
+        int before = Integer.MIN_VALUE, zeroIndex = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (before > nums[i]) {
+                zeroIndex = i;
+                break;
+            }
+
+            before = nums[i];
+            if (before == target) return true;
+        }
+
         int startIdx = 0, endIdx = nums.length - 1, targetIdx = 0;
 
         while (startIdx <= endIdx) {
