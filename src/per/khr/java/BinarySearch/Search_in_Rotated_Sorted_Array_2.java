5172eac3e82041c3312f9a899d2b83db3f48f991
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-07-01 21:53:25 +0900

    Medium -> 81. Search in Rotated Sorted Array 2

---
+++ src/per/khr/java/BinarySearch/Search_in_Rotated_Sorted_Array_2.java
@@ -0,0 +1,31 @@
+package per.khr.java.BinarySearch;
+
+import java.util.Arrays;
+
+/**
+ * 우선 0의 위치를 찾고 target이 첫번째 인덱스부터 0의 -1 위치까지에 속하는 지
+ * 그 다음에 속하는 지를 알아야 한다.
+ * 마이너스가 나올 수 있음.
+ * return type이 boolean이기 때문에 가장 쉬운 방법은 nums를 sorting한 후에 이분 탐색으로 찾는 방법.
+ * 하지만 찾는 숫자의 index를 요구한다면?
+ */
+public class Search_in_Rotated_Sorted_Array_2 {
+    public boolean search(int[] nums, int target) {
+        Arrays.sort(nums);
+        int startIdx = 0, endIdx = nums.length - 1, targetIdx = 0;
+
+        while (startIdx <= endIdx) {
+            targetIdx = startIdx + (endIdx - startIdx) / 2;
+
+            if (nums[targetIdx] == target) {
+                return true;
+            } else if (nums[targetIdx] > target) {
+                endIdx = targetIdx - 1;
+            } else if (nums[targetIdx] < target) {
+                startIdx = targetIdx + 1;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/Search_in_Rotated_Sorted_Array_2Test.java
@@ -0,0 +1,16 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+import per.khr.java.BinarySearch.Search_in_Rotated_Sorted_Array_2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Search_in_Rotated_Sorted_Array_2Test extends TestCase {
+
+    @Test
+    public void testSearch() {
+        Search_in_Rotated_Sorted_Array_2 sirsa2 = new Search_in_Rotated_Sorted_Array_2();
+
+        assertThat(sirsa2.search(new int[] {2,5,6,0,0,1,2}, 0)).isEqualTo(true);
+        assertThat(sirsa2.search(new int[] {2,5,6,0,0,1,2}, 3)).isEqualTo(false);
+    }
+}
