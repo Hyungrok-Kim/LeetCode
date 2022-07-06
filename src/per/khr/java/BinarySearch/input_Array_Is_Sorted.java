b02b198c303f2047ee44853a7c3367dde4c08cdf
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-07-06 19:51:34 +0900

    Medium -> 167. Two Sum 2 - Input Array Is Sorted

--- src/per/khr/java/BinarySearch/input_Array_Is_Sorted.java
+++ src/per/khr/java/BinarySearch/input_Array_Is_Sorted.java
@@ -7,12 +7,12 @@ public class input_Array_Is_Sorted {
     /**
      * 8ms
      * faster than 9.05%
+     *
      * @param numbers
      * @param target
      * @return
      */
     public int[] twoSum(int[] numbers, int target) {
-
         for (int i = 0, limit = numbers.length; i < limit; ++i) {
             int findNum = target - numbers[i];
             int start = 0, end = numbers.length - 1;
@@ -36,5 +36,28 @@ public class input_Array_Is_Sorted {
         }
 
         return null;
+    }
+
+    /**
+     * 3ms
+     * faster than 35.94%
+     *
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] numbers, int target) {
+        int start = 0, end = numbers.length - 1;
+
+        while (start < end) {
+            if (numbers[start] + numbers[end] == target)
+                return new int[]{start + 1, end + 1};
+            else if (numbers[start] + numbers[end] < target)
+                start++;
+            else
+                end--;
+        }
+
+        return new int[]{-1, -1};
     }
 }
--- src/test/input_Array_Is_SortedTest.java
+++ src/test/input_Array_Is_SortedTest.java
@@ -10,8 +10,11 @@ public class input_Array_Is_SortedTest extends TestCase {
     public void testTwoSum() {
         input_Array_Is_Sorted iais = new input_Array_Is_Sorted();
 
-        assertThat(iais.twoSum(new int[]{2, 7, 11, 15}, 9)).isEqualTo(new int[]{1, 2});
-        assertThat(iais.twoSum(new int[]{2, 3, 4}, 6)).isEqualTo(new int[]{1, 3});
-        assertThat(iais.twoSum(new int[]{-1, 0}, -1)).isEqualTo(new int[]{1, 2});
+        assertThat(iais.twoSum2(new int[]{-1000, -1, 0, 1}, 1)).isEqualTo(new int[]{3, 4});
+        assertThat(iais.twoSum2(new int[]{2, 3, 4}, 6)).isEqualTo(new int[]{1, 3});
+        assertThat(iais.twoSum2(new int[]{1, 3, 4, 4}, 8)).isEqualTo(new int[]{3, 4});
+        assertThat(iais.twoSum2(new int[]{0, 0, 3, 4}, 0)).isEqualTo(new int[]{1, 2});
+        assertThat(iais.twoSum2(new int[]{2, 7, 11, 15}, 9)).isEqualTo(new int[]{1, 2});
+        assertThat(iais.twoSum2(new int[]{-1, 0}, -1)).isEqualTo(new int[]{1, 2});
     }
 }
