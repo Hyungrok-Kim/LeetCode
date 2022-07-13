af01cc4b681e07f47b10e866510cdf9eb4478acf
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-07-13 23:23:09 +0900

    Medium -> 130. Surrounded Regions

--- src/per/khr/java/BFSDFS/Surrounded_Regions.java
+++ src/per/khr/java/BFSDFS/Surrounded_Regions.java
@@ -9,48 +9,39 @@ package per.khr.java.BFSDFS;
  * ]
  */
 public class Surrounded_Regions {
-    private static final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private static final int[][] checks = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public void solve(char[][] board) {
-        if (board.length == 0)
-            return;
+        if (board.length == 0) return;
 
-        int rows = board.length, cols = board[0].length;
+        int xLen = board.length, yLen = board[0].length;
 
-        for (int i = 0; i < rows; i++) {
-            if (board[i][0] == 'O')
-                markNotSurrounded(i, 0, board);
-            if (board[i][cols - 1] == 'O')
-                markNotSurrounded(i, cols - 1, board);
+        for (int i = 0; i < yLen; ++i) {
+            if (board[i][0] == 'O') dfs(0, i, board, xLen, yLen);
+            if (board[i][xLen - 1] == 'O') dfs(xLen - 1, i, board, xLen, yLen);
         }
 
-        for (int j = 0; j < cols; j++) {
-            if (board[0][j] == 'O')
-                markNotSurrounded(0, j, board);
-            if (board[rows - 1][j] == 'O')
-                markNotSurrounded(rows - 1, j, board);
+        for (int j = 0; j < xLen; ++j) {
+            if (board[0][j] == 'O') dfs(j, 0, board, xLen, yLen);
+            if (board[yLen - 1][j] == 'O') dfs(j, yLen - 1, board, xLen, yLen);
         }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-
-                if (board[i][j] == '*') // Restores '*' to 'O'
-                    board[i][j] = 'O';
-                else if (board[i][j] == 'O') // Captures 'O' surrounded by 'X'
-                    board[i][j] = 'X';
+        for (int i = 0; i < yLen; ++i) {
+            for (int j = 0; j < xLen; ++j) {
+                if (board[i][j] == '*') board[i][j] = 'O';
+                else if (board[i][j] == 'O') board[i][j] = 'X';
             }
         }
     }
 
-    // Mark 'O' not surrounded by 'X' as '*'
-    private void markNotSurrounded(int x, int y, char[][] board) {
-        board[x][y] = '*';
-        for (int[] dir : directions) {
-            int nx = x + dir[0], ny = y + dir[1];
-            if (nx >= 0 && nx < board.length
-                    && ny >= 0 && ny < board[0].length
-                    && board[nx][ny] == 'O') {
-                markNotSurrounded(nx, ny, board);
+    public void dfs(int x, int y, char[][] board, int xLen, int yLen) {
+        board[y][x] = '*';
+
+        for (int[] check : checks) {
+            if (0 <= x + check[1] && x + check[1] < xLen &&
+                    0 <= y + check[0] && y + check[0] < yLen
+                    && board[y + check[0]][x + check[1]] == 'O') {
+                dfs(x + check[1], y + check[0], board, xLen, yLen);
             }
         }
     }
