1fe828bef9975b0002a2453ace5610ad8e19866e
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-07-17 23:32:17 +0900

    Medium -> 200. Number of Islands

--- src/per/khr/java/BFSDFS/Number_of_Islands.java
+++ src/per/khr/java/BFSDFS/Number_of_Islands.java
@@ -1,5 +1,9 @@
 package per.khr.java.BFSDFS;
 
+/**
+ * 붙어있는 섬의 갯수가 몇 개인지 찾는문제.
+ * 당연히 DFS로 풀면 될 듯.
+ */
 public class Number_of_Islands {
     public int numIslands(char[][] grid) {
         return 0;
