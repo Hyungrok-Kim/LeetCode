0cd8d88fb0c41222b213bf35ca5fab9c8b8fedcf
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-07-19 20:49:00 +0900

    297. Serialize and Deserialize Binary Tree

--- src/per/khr/java/BFSDFS/Serialize_and_Deserialize_Binary_Tree.java
+++ src/per/khr/java/BFSDFS/Serialize_and_Deserialize_Binary_Tree.java
@@ -2,16 +2,45 @@ package per.khr.java.BFSDFS;
 
 import per.khr.java.vo.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * BFS를 구현하는 문제?
  * BFS는 어떻게 구현하느냐..
  * visited Array와 needVisit Stack을 이용해서 구현하면 될 듯..?
- *
  */
 public class Serialize_and_Deserialize_Binary_Tree {
     public String serialize(TreeNode root) {
+        if (root == null) return null;
 
-        return null;
+        ArrayList<String> visited = new ArrayList<>();
+        Queue<TreeNode> needVisit = new LinkedList<>();
+
+        needVisit.offer(root);
+
+        while (!needVisit.isEmpty()) {
+            TreeNode target = needVisit.poll();
+
+            if (target == null) visited.add("null");
+            else {
+                visited.add(String.valueOf(target.val));
+
+                if (target.left == null) needVisit.offer(null);
+                else needVisit.offer(target.left);
+
+                if (target.right == null) needVisit.offer(null);
+                else needVisit.offer(target.right);
+            }
+        }
+
+        for (int i = visited.size() - 1; i >= 0; --i) {
+            if (visited.get(i).equals("null")) visited.remove(i);
+            else break;
+        }
+
+        return visited.toString();
     }
 
     public TreeNode deserialize(String data) {
--- src/test/Serialize_and_Deserialize_Binary_TreeTest.java
+++ src/test/Serialize_and_Deserialize_Binary_TreeTest.java
@@ -12,7 +12,7 @@ public class Serialize_and_Deserialize_Binary_TreeTest extends TestCase {
         TreeNode param = new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4), new TreeNode(5)));
         Serialize_and_Deserialize_Binary_Tree sadbt = new Serialize_and_Deserialize_Binary_Tree();
 
-        assertThat(sadbt.serialize(param)).isEqualTo("1,2,3,null,null,4,5");
+        assertThat(sadbt.serialize(param)).isEqualTo("[1,2,3,null,null,4,5]");
     }
 
     public void testDeserialize() {
