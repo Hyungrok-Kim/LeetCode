10f52dd20b53a060f50008c62ed30f77cf8edf33
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-07-21 20:36:18 +0900

    Medium -> 310. Minimum Height Trees

---
+++ src/per/khr/java/BFSDFS/Minimum_Height_Trees.java
@@ -0,0 +1,12 @@
+package per.khr.java.BFSDFS;
+
+import java.util.List;
+
+/**
+ * 트리가 순서가 없으니까.. 모든 트리를 만들어보고 가장 낮은 높이를 만드는 트리들의 root 번호를 List 타입으로 return 하라?
+ */
+public class Minimum_Height_Trees {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        return null;
+    }
+}
