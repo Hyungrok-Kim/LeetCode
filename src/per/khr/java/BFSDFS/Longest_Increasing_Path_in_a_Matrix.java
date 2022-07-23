1f58463d7cb23208c457af091995c7913c870baf
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-07-23 13:31:43 +0900

    Hard -> 329. Longest Increasing Path in a Matrix

--- src/per/khr/java/BFSDFS/Longest_Increasing_Path_in_a_Matrix.java
+++ src/per/khr/java/BFSDFS/Longest_Increasing_Path_in_a_Matrix.java
@@ -4,10 +4,10 @@ import java.util.TreeSet;
 
 /**
  * 낮은 수에서 높은 수로만 갈 수 있는데 가장 많이 가야 한다.
+ * 이렇게 풀면 모든 경우의 수를 다 보기 때문에 시간복잡도를 통과할 수 없음..
  */
 public class Longest_Increasing_Path_in_a_Matrix {
     private static final int[][] moves = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
     public void dfs(int i, int j, int[][] matrix, int moveCount, TreeSet<Integer> ts) {
         int target = matrix[i][j];
 //        matrix[i][j] = 0;
@@ -27,7 +27,6 @@ public class Longest_Increasing_Path_in_a_Matrix {
             }
         }
     }
-
     public int longestIncreasingPath(int[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) return 0;
 
