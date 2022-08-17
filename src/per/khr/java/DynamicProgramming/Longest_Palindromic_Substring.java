40295e68482996330952e7106babfa913a4cca65
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-08-17 19:32:02 +0900

    Medium -> 5. Longest Palindromic Substring

---
+++ src/per/khr/java/DynamicProgramming/Longest_Palindromic_Substring.java
@@ -0,0 +1,13 @@
+package per.khr.java.DynamicProgramming;
+
+/**
+ * 앞으로 읽으나 뒤로 읽으나 똑같은 것을 Palindromic이라고 한다.
+ * String s를 Parameter로 받아 가장 긴 Palindromic한 문자열을 return해주면 됨.
+ */
+public class Longest_Palindromic_Substring {
+    public String longestPalindrome(String s) {
+
+
+        return null;
+    }
+}
