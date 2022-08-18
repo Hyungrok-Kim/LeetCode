f434276b055b7d68667e744d67b9f785a342c0d2
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-08-18 21:50:30 +0900

    Easy -> 1. Two Sum

--- src/per/khr/java/Hash/Two_Sum.java
+++ src/per/khr/java/Hash/Two_Sum.java
@@ -1,8 +1,16 @@
 package per.khr.java.Hash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Two_Sum {
+    /**
+     * 169ms
+     * @param nums
+     * @param target
+     * @return
+     */
     public static int[] twoSum(int[] nums, int target) {
-//      내 답안 169 ms
         int[] result = new int[2];
 
         int idx = 0;
@@ -16,25 +24,26 @@ public class Two_Sum {
                     return new int[]{idx - 1, idx2};
             }
         }
-
-//        빠른 답안 (HashMap 사용..)
-//        int[] result = new int[2];
-//        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-//        for (int i = 0; i < nums.length; i++) {
-//            if (map.containsKey(target - nums[i])) {
-//                result[1] = i;
-//                result[0] = map.get(target - nums[i]);
-//                return result;
-//            }
-//            map.put(nums[i], i);
-//        }
-//        return result;
     }
 
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
+    /**
+     * 4ms
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                return result;
+            }
+            map.put(nums[i], i);
+        }
 
-        System.out.println(twoSum(nums, target));
+        return result;
     }
 }
