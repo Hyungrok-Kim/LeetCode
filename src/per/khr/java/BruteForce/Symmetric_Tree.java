d4a94b19edfae12f28bd6ae95bb690872aa98cdf
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-08-18 21:40:12 +0900

    Easy -> 101. Symmetric Tree

--- src/per/khr/java/BruteForce/Symmetric_Tree.java
+++ src/per/khr/java/BruteForce/Symmetric_Tree.java
@@ -4,11 +4,11 @@ import per.khr.java.vo.TreeNode;
 
 public class Symmetric_Tree {
     public boolean recursive(TreeNode q, TreeNode p) {
-        if(q == null && p != null) return false;
-        if(q != null && p == null) return false;
-        if(q == null && p == null) return true;
+        if (q == null && p != null) return false;
+        if (q != null && p == null) return false;
+        if (q == null && p == null) return true;
 
-        if(q.val != p.val) return false;
+        if (q.val != p.val) return false;
 
         return recursive(q.left, p.right) && recursive(q.right, p.left);
     }
