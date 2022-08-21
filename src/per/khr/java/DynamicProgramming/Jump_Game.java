0b694b363d64426d4996062a066088d6333ebfc7
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-08-21 22:52:47 +0900

    Medium -> 55. Jump Game

--- src/per/khr/java/DynamicProgramming/Jump_Game.java
+++ src/per/khr/java/DynamicProgramming/Jump_Game.java
@@ -1,9 +1,12 @@
 package per.khr.java.DynamicProgramming;
 
+/**
+ * Zero-index 시작.
+ * 최대 nums[idx]만큼 점프 가능.
+ * 마지막 idx에 도달할 수 있는가.
+ */
 public class Jump_Game {
     public boolean canJump(int[] nums) {
-
-
         return true;
     }
 }
